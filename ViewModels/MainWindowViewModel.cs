a1db7a15deda7a066ca4f489262fbf06a6269ec8
Tofame <80583148+Tofame@users.noreply.github.com>
2026-07-04 11:55:53 +0200

    init with Avalonia MVVM

---
+++ ViewModels/MainWindowViewModel.cs
@@ -0,0 +1,6 @@
+﻿namespace NyxAssetsEditor.ViewModels;
+
+public partial class MainWindowViewModel : ViewModelBase
+{
+    public string Greeting { get; } = "Welcome to Avalonia!";
+}
---
+++ ViewModels/ViewModelBase.cs
@@ -0,0 +1,7 @@
+﻿using CommunityToolkit.Mvvm.ComponentModel;
+
+namespace NyxAssetsEditor.ViewModels;
+
+public abstract class ViewModelBase : ObservableObject
+{
+}
